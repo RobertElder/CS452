5d9aee5cdee8f67bc08f2d91e5bc74a5e3e328e9
Robert Elder <robert@robertelder.ca>
2013-05-18 22:56:06 -0400

    Closer to a testable implentation of calling kernel functions.

--- io/kernel1/kernel_entry.s
+++ io/kernel1/kernel_entry.s
@@ -3,13 +3,18 @@
 .extern k_MyParentTid
 .extern k_Pass
 .extern k_Exit
-LDR r10, [lr,#(-4)]; /*  Put the SWI instruction in R10 */
+
+/* TODO: Need instructions to pop kernel state off of the stack here. Make sure we don't overwrite r10 */
+
+
+
+LDR r10, [lr,#(-4)]; /*  Put the SWI instruction call in R10 */
 MOV r10, r10, LSL #16; /*  Get rid of the high 16 bits by doing a left logical shift of 16 to discard the high bits  */
-MOV r10, r10, LSR #14; /*  Get the function id times four, so we can jump to the correct branch location */
+MOV r10, r10, LSR #14; /*  Only shift back 14 to get the function id times four, so we can jump to the correct branch location */
 ADD PC, PC, r10; /*  Use the function id times four to jump to the correct branch for our kernel function */
-BL k_Create
-BL k_MyTid
-BL k_MyParentTid
-BL k_Pass
-BL k_Exit
+B k_Create
+B k_MyTid
+B k_MyParentTid
+B k_Pass
+B k_Exit
 
--- io/kernel1/private_kernel_interface.c
+++ io/kernel1/private_kernel_interface.c
@@ -1,21 +1,30 @@
 #include "private_kernel_interface.h"
 
+void asm_KernelExit();
+
 int k_Create( int priority, void (*code)( ) ){
+	/* TODO Argument passing has not been thought through yet. */
+	asm_KernelExit();
 	return 0;
 }
 
 int k_MyTid(){
+	/* TODO Argument passing has not been thought through yet. */
+	asm_KernelExit();
 	return 0;
 }
 
 int k_MyParentTid(){
-	return 0;
+	/* TODO Argument passing has not been thought through yet. */
+	asm_KernelExit();
 }
 
 void k_Pass(){
-
+	/* TODO Argument passing has not been thought through yet. */
+	asm_KernelExit();
 }
 
 void k_Exit(){
-
+	/* TODO Argument passing has not been thought through yet. */
+	asm_KernelExit();
 }
