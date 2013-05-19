24a9ae17d30152d402c6c04f10ddbf558395e2a1
Christopher Foo <chris.foo@gmail.com>
2013-05-19 16:24:20 -0400

    Adds empty task_descriptor.{ch}

---
+++ io/kernel1/task_descriptor.h
@@ -0,0 +1,26 @@
+#include "queue.h"
+
+#ifndef TASK_DESCRIPTOR_H_
+#define TASK_DESCRIPTOR_H_
+
+typedef enum TaskState {
+	READY,
+	ACTIVE,
+	ZOMBIE,
+	SEND_BLOCKED,
+	RECEIVE_BLOCKED,
+	REPLY_BLOCKED,
+	EVENT_BLOCKED
+} TaskState;
+
+typedef struct TaskDescriptor {
+	int id;
+	TaskState state;
+	QueuePriority priority;
+	int parent_id;
+	int * stack_pointer;
+	int * spsr;
+	int return_value;
+} TD;
+
+#endif /* TASK_DESCRIPTOR_H_ */
