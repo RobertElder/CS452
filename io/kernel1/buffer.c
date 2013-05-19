597cf8e33858cc877645243980ee2ac7bc97af91
Christopher Foo <chris.foo@gmail.com>
2013-05-19 15:06:16 -0400

    Adds buffer.{ch}

---
+++ io/kernel1/buffer.c
@@ -0,0 +1,27 @@
+#include "buffer.h"
+
+void Buffer_Initialize(Buffer * buffer) {
+	buffer->start = 0;
+	buffer->end = 0;
+}
+
+int Buffer_PutChar(Buffer * buffer, char c) {
+	if ((buffer->end + 1) % buffer->size == buffer->start) {
+		return ERR_BUFFER_FULL;
+	}
+
+	buffer->array[buffer->end] = c;
+	buffer->end = (buffer->end + 1) % buffer->size;
+
+	return 0;
+}
+
+char Buffer_GetChar(Buffer * buffer) {
+	if (buffer->start == buffer->end) {
+		return 0;
+	}
+
+	unsigned char c = buffer->array[buffer->end];
+	buffer->start = (buffer->start + 1) % buffer->size;
+	return c;
+}
---
+++ io/kernel1/buffer.h
@@ -0,0 +1,21 @@
+#ifndef BUFFER_H_
+#define BUFFER_H_
+
+const int ERR_BUFFER_FULL = -1;
+
+typedef struct Buffer {
+	unsigned char * array;
+	unsigned int start;
+	unsigned int end;
+	unsigned int size;
+} Buffer;
+
+void Buffer_Initialize(Buffer * buffer);
+
+// Put a character. Returns 0 if ok.
+int Buffer_PutChar(Buffer * buffer, char c);
+
+//  Remove one character from the buffer we created.  Return null on empty buffer.
+char Buffer_GetChar(Buffer * buffer);
+
+#endif /* BUFFER_H_ */
