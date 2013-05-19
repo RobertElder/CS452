15b9cbc09e49c2791960152827e9f87804cab8ef
Robert Elder <robert@robertelder.ca>
2013-05-19 14:31:33 -0400

    Made progress on getting transition to supervisor mode working.

--- io/kernel1/kernel_entry.s
+++ io/kernel1/kernel_entry.s
@@ -3,11 +3,11 @@
 .extern k_MyParentTid
 .extern k_Pass
 .extern k_Exit
+.global asm_SwiCallEntry
 
+asm_SwiCallEntry:
 /* TODO: Need instructions to pop kernel state off of the stack here. Make sure we don't overwrite r10 */
-
-
-
+B asm_SwiCallEntry
 LDR r9, [lr,#(-4)]; /*  Put the SWI instruction call in R10.  This contains the kernel function id.  */
 MOV r9, r9, LSL #8; /*  Get rid of the high 8 bits by doing a left logical shift of 16 to discard the high bits  */
 MOV r9, r9, LSR #6; /*  Only shift back 6 to get the function id times four, so we can jump to the correct branch location */
