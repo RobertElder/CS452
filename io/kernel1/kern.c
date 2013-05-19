499def3dac1c02492b681f545bb6bb91bd4d7d47
Robert Elder <robert@robertelder.ca>
2013-05-19 14:31:33 -0400

    Made progress on getting transition to supervisor mode working.

--- io/kernel1/kern.c
+++ io/kernel1/kern.c
@@ -1,6 +1,15 @@
 
 #include "public_kernel_interface.h"
 
+void asm_SwiCallEntry();
+
 int main(){
-	return 1;
+	int * first_swi_instruction_address = (int *)(0x8);
+	int * swi_call_entry_address = (int *)(0x20);
+	void (*swi_entry)() = asm_SwiCallEntry;
+	*swi_call_entry_address = (int)swi_entry;
+	//  Encodes the instruction for ldr  pc, [pc, #0x18]
+	*first_swi_instruction_address = 0xE59FF018;
+	//Create( 1, 0);
+	return 0;
 }
