9503c3455cd8650dccfd26cb83ccfee54e49c184
Robert Elder <robert@robertelder.ca>
2013-05-19 22:08:30 -0400

    Lots of kernel entry stuff unimplemented.  Able to call all kernel functions and print a message from within.

--- io/kernel1/kernel_entry.s
+++ io/kernel1/kernel_entry.s
@@ -12,6 +12,7 @@ LDR r9, [lr,#-4]; /*  Put the SWI instruction call in R9.  This contains the ker
 MOV r9, r9, LSL #8; /*  Get rid of the high 8 bits by doing a left logical shift of 16 to discard the high bits  */
 MOV r9, r9, LSR #6; /*  Only shift back 6 to get the function id times four, so we can jump to the correct branch location */
 ADD PC, PC, r9; /*  Use the function id times four to jump to the correct branch for our kernel function */
+ADD PC, PC, r9; /*  Dummy instruction that does not execute because we jump over it so the jump table works correctly */
 /*  Do branches without link because the link register (R14_svc) currently stores the return address immediately after the SWI instruction */
 B k_Create
 B k_MyTid
--- io/kernel1/private_kernel_interface.c
+++ io/kernel1/private_kernel_interface.c
@@ -5,27 +5,33 @@ void asm_KernelExit();
 
 int k_Create( int priority, void (*code)( ) ){
 	/* TODO Argument passing has not been thought through yet. */
+	robputstrbusy((const unsigned char *)"In function k_Create.\n");
 	asm_KernelExit();
 	return 0; /* Needed to get rid of compiler warnings only.  Execution does not reach here */
 }
 
 int k_MyTid(){
 	/* TODO Argument passing has not been thought through yet. */
+	robputstrbusy((const unsigned char *)"In function k_MyTid\n");
 	asm_KernelExit();
 	return 0; /* Needed to get rid of compiler warnings only.  Execution does not reach here */
 }
 
 int k_MyParentTid(){
 	/* TODO Argument passing has not been thought through yet. */
+	robputstrbusy((const unsigned char *)"In function k_MyParentTid\n");
 	asm_KernelExit();
+	return 0; /* Needed to get rid of compiler warnings only.  Execution does not reach here */
 }
 
 void k_Pass(){
 	/* TODO Argument passing has not been thought through yet. */
+	robputstrbusy((const unsigned char *)"In function k_Pass\n");
 	asm_KernelExit();
 }
 
 void k_Exit(){
 	/* TODO Argument passing has not been thought through yet. */
+	robputstrbusy((const unsigned char *)"In function k_Exit\n");
 	asm_KernelExit();
 }
