47a2295788e6264b2872c6753c13e94994d89345
Robert Elder <robert@robertelder.ca>
2013-05-20 14:14:51 -0400

    Added more complex operations inside kernel functions.

--- io/kernel1/kernel_state.h
+++ io/kernel1/kernel_state.h
@@ -5,7 +5,10 @@
 #define KERNEL_STATE_H_
 
 typedef struct KernelState {
+	/* The maximum number of tasks that the kernel currently supports. */
 	unsigned int max_tasks;
+	/* The last value of the kernel stack pointer.  This will always be the same value just after entering a kernel function. */
+	void * last_kernel_sp;
 } KernelState;
 
 #endif /* KERNEL_STATE_H_ */
--- io/kernel1/private_kernel_interface.c
+++ io/kernel1/private_kernel_interface.c
@@ -4,50 +4,72 @@
 
 void asm_KernelExit();
 
+void print_kernel_state(KernelState * k_state){
+	robprintfbusy((const unsigned char *)"Max tasks: %d.\n",k_state->max_tasks);
+	robprintfbusy((const unsigned char *)"Last Kernel SP value: %x.\n",k_state->last_kernel_sp);
+}
+
 void k_InitKernel(){
+	//  Now initialize the kernel stack pointer to the correct value
+	//  The kernel state struct is stored at the base of the kernel stack, account for this.
+	register int *current_kernel_stack_pointer asm ("r13");
+        *current_kernel_stack_pointer = (int)(KERNEL_STACK_START - sizeof(KernelState));
+
 	//  Directly set the kernel state structure values on the stack.
 	KernelState * k_state = (KernelState *) KERNEL_STACK_START;
-	k_state->max_tasks = 300;
-	//  Now initialize the stack pointer to the correct value
-	register int *current_kernel_stack_pointer asm ("r13");
-	//  The kernel state struct is stored at the base of the kernel stack.
-	*current_kernel_stack_pointer = KERNEL_STACK_START - sizeof(KernelState);
-	robputstrbusy((const unsigned char *)"Current kernel stack pointer:\n");
-	robputrbusy(*current_kernel_stack_pointer);
+	k_state->max_tasks = 4;
+	k_state->last_kernel_sp = (void*)*current_kernel_stack_pointer;
+
+	//  Now print out some useful info
+	robprintfbusy((const unsigned char *)"Inside kernel init function. SP is %x\n",*current_kernel_stack_pointer);
+	print_kernel_state(k_state);
 	asm_KernelExit();
 }
 
 int k_Create( int priority, void (*code)( ) ){
 	KernelState * k_state = (KernelState *) KERNEL_STACK_START;
-	robprintfbusy((const unsigned char *)"Max tasks is %d.\n",k_state->max_tasks);
-	/* TODO Argument passing has not been thought through yet. */
-	robputstrbusy((const unsigned char *)"In function k_Create\n");
+	register int *current_kernel_stack_pointer asm ("r13");
+        *current_kernel_stack_pointer = (int )k_state->last_kernel_sp;
+	robprintfbusy((const unsigned char *)"In function k_Create. Priority is %d, code is %x\n",priority, code);
+	print_kernel_state(k_state);
 	asm_KernelExit();
 	return 0; /* Needed to get rid of compiler warnings only.  Execution does not reach here */
 }
 
 int k_MyTid(){
-	/* TODO Argument passing has not been thought through yet. */
-	robputstrbusy((const unsigned char *)"In function k_MyTid\n");
+	KernelState * k_state = (KernelState *) KERNEL_STACK_START;
+	register int *current_kernel_stack_pointer asm ("r13");
+        *current_kernel_stack_pointer = (int )k_state->last_kernel_sp;
+	robprintfbusy((const unsigned char *)"In function k_MyTid\n");
+	print_kernel_state(k_state);
 	asm_KernelExit();
 	return 0; /* Needed to get rid of compiler warnings only.  Execution does not reach here */
 }
 
 int k_MyParentTid(){
-	/* TODO Argument passing has not been thought through yet. */
-	robputstrbusy((const unsigned char *)"In function k_MyParentTid\n");
+	KernelState * k_state = (KernelState *) KERNEL_STACK_START;
+	register int *current_kernel_stack_pointer asm ("r13");
+        *current_kernel_stack_pointer = (int )k_state->last_kernel_sp;
+	robprintfbusy((const unsigned char *)"In function k_MyParentTid\n");
+	print_kernel_state(k_state);
 	asm_KernelExit();
 	return 0; /* Needed to get rid of compiler warnings only.  Execution does not reach here */
 }
 
 void k_Pass(){
-	/* TODO Argument passing has not been thought through yet. */
-	robputstrbusy((const unsigned char *)"In function k_Pass\n");
+	KernelState * k_state = (KernelState *) KERNEL_STACK_START;
+	register int *current_kernel_stack_pointer asm ("r13");
+        *current_kernel_stack_pointer = (int )k_state->last_kernel_sp;
+	robprintfbusy((const unsigned char *)"In function k_Pass\n");
+	print_kernel_state(k_state);
 	asm_KernelExit();
 }
 
 void k_Exit(){
-	/* TODO Argument passing has not been thought through yet. */
-	robputstrbusy((const unsigned char *)"In function k_Exit\n");
+	KernelState * k_state = (KernelState *) KERNEL_STACK_START;
+	register int *current_kernel_stack_pointer asm ("r13");
+        *current_kernel_stack_pointer = (int )k_state->last_kernel_sp;
+	robprintfbusy((const unsigned char *)"In function k_Exit\n");
+	print_kernel_state(k_state);
 	asm_KernelExit();
 }
