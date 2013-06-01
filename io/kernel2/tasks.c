8687896128f9a2907ead4d389ec7f9a39cbd6df6
Robert Elder <robert@robertelder.ca>
2013-06-01 15:45:44 -0400

    Added timing to tasks.

--- io/kernel2/tasks.c
+++ io/kernel2/tasks.c
@@ -1,3 +1,4 @@
+#include <ts7200.h>
 #include "tasks.h"
 #include "public_kernel_interface.h"
 #include "robio.h"
@@ -9,50 +10,35 @@
 #include "rps.h"
 
 
-void print_loop_timing(ChannelDescription * terminal_channel, unsigned int observed_val, unsigned int last_timer_value, unsigned int cycles_per_tick, unsigned int max_time){
-	robprintfbusy( "Timer observed: %d\n", observed_val );
-	robprintfbusy( "Last Timer: %d\n", last_timer_value );
+void print_loop_timing(unsigned int observed_val, unsigned int last_timer_value, unsigned int cycles_per_tick, unsigned int max_time){
+	robprintfbusy( (const unsigned char *)"Timer observed: %d\n", observed_val );
+	robprintfbusy( (const unsigned char *)"Last Timer: %d\n", last_timer_value );
 	unsigned int diff = observed_val > last_timer_value ? (cycles_per_tick - observed_val) + last_timer_value : last_timer_value - observed_val;
-	robprintfbusy( "Time in loop: max: %dus, last: %dus.\n", (int)((double)max_time/((double)cycles_per_tick/(double)100000)), (int)((double)diff/((double)cycles_per_tick/(double)100000)));
+	robprintfbusy( (const unsigned char *)"Time in loop: max: %dus, last: %dus.\n", (int)((double)max_time/((double)cycles_per_tick/(double)100000)), (int)((double)diff/((double)cycles_per_tick/(double)100000)));
 }
 
 unsigned int max_time = 0;
 
-int * timer_ldr = (TIMER3_BASE + LDR_OFFSET);
-int * timer_val = (TIMER3_BASE + VAL_OFFSET);
-int * timer_ctrl = (TIMER3_BASE + CRTL_OFFSET);
 
-
+#define CYCLES_PER_TICK 50800
 // 508000 cycles per second, means a tick (100ms) has 50800 cycles
-unsigned int cycles_per_tick = 50800;
-int last_timer_value = cycles_per_tick;
+unsigned int cycles_per_tick = CYCLES_PER_TICK;
+int last_timer_value = CYCLES_PER_TICK;
 int ticks = 0;
 
 
-//  Disable the timer before we set the load value
-*timer_ctrl = (*timer_ctrl) ^ ENABLE_MASK;
-*timer_ldr = cycles_per_tick;
-//  Turn the timer on enabled, with clock 508khz and periodic mode
-*timer_ctrl = ENABLE_MASK | CLKSEL_MASK | MODE_MASK;
 
 
 
-//  On send
-int observed_val = *timer_val;
-
-//  On finished:
-unsigned int diff = observed_val > last_timer_value ? (cycles_per_tick - observed_val) + last_timer_value : last_timer_value - observed_val;
-if(diff > max_time)
-	max_time = diff;
-if(observed_val > last_timer_value){
-	ticks++;
-	print_loop_timing(&terminal_channel, observed_val, last_timer_value, cycles_per_tick, max_time);
-}
 
 void KernelTask_Start() {
 	int tid = Create(NORMAL, &FirstTask_Start);
 	
 	assert(tid == 1, "FirstTask tid not 1");
+
+
+
+
 	
 	Exit();
 	assert(0, "Shouldn't see me\n");
@@ -60,7 +46,6 @@ void KernelTask_Start() {
 
 void FirstTask_Start() {
 	int tid;
-	const int num_clients = 3;
 	
 	tid = Create(NORMAL, &SecondTask_Start);
 	assert(tid == 2, "second task tid not 2");
@@ -80,10 +65,10 @@ void FirstTask_Start() {
 		}
 
 		assert(receive_message->message_type == MESSAGE_TYPE_PLAY, "1st task didn't get PLAY message");
-		robprintfbusy((const unsigned char *)"First: received\n");
+		//robprintfbusy((const unsigned char *)"First: received\n");
 		reply_message->message_type = MESSAGE_TYPE_ACK;
 		Reply(2, reply_buffer, MESSAGE_SIZE);
-		robprintfbusy((const unsigned char *)"First: replied\n");
+		//robprintfbusy((const unsigned char *)"First: replied\n");
 		Pass();
 	}
 	
@@ -99,6 +84,17 @@ void FirstTask_Start() {
 }
 
 void SecondTask_Start() {
+
+	int * timer_ldr = (int *)(TIMER3_BASE + LDR_OFFSET);
+	int * timer_val = (int *)(TIMER3_BASE + VAL_OFFSET);
+	int * timer_ctrl = (int *)(TIMER3_BASE + CRTL_OFFSET);
+
+	//  Disable the timer before we set the load value
+	*timer_ctrl = (*timer_ctrl) ^ ENABLE_MASK;
+	*timer_ldr = cycles_per_tick;
+	//  Turn the timer on enabled, with clock 508khz and periodic mode
+	*timer_ctrl = ENABLE_MASK | CLKSEL_MASK | MODE_MASK;
+
 	char send_buffer[MESSAGE_SIZE];
 	char reply_buffer[MESSAGE_SIZE];
 	TestMessage * send_message = (TestMessage *) send_buffer;
@@ -106,9 +102,19 @@ void SecondTask_Start() {
 	unsigned int count = 0;
 	while (count < 100) {
 		send_message->message_type = MESSAGE_TYPE_PLAY;
+		// start
+		int last_timer_value = *timer_val;
 		Send(1, send_buffer, MESSAGE_SIZE, reply_buffer, MESSAGE_SIZE);
+		int observed_val = *timer_val;
+		//  On finished:
+		unsigned int diff = observed_val > last_timer_value ? (cycles_per_tick - observed_val) + last_timer_value : last_timer_value - observed_val;
+		if(diff > max_time)
+			max_time = diff;
+		ticks++;
+		print_loop_timing(observed_val, last_timer_value, cycles_per_tick, max_time);
+
 		assert(reply_message->message_type == MESSAGE_TYPE_ACK, "second task didn't get an ACK message");
-		robprintfbusy((const unsigned char *)"Second: sent\n");
+		//robprintfbusy((const unsigned char *)"Second: sent\n");
 		Pass();
 		count ++;
 	}
