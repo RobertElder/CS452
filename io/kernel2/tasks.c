d12bd867b7d4f8ff404065de61c3497a8862174c
Christopher Foo <chris.foo@gmail.com>
2013-06-01 14:59:37 -0400

    Add in base code for task

--- io/kernel2/tasks.c
+++ io/kernel2/tasks.c
@@ -21,22 +21,66 @@ void FirstTask_Start() {
 	int tid;
 	const int num_clients = 3;
 	
-	tid = Create(NORMAL, &NameServer_Start);
-	assert(tid == 2, "NameServer tid not 2");
+	tid = Create(NORMAL, &SecondTask_Start);
+	assert(tid == 2, "second task tid not 2");
 	
-	tid = Create(NORMAL, &RPSServer_Start);
-	assert(tid == 3, "RPServer tid not 3");
-	
-	int i;
-	for (i = 0; i < num_clients; i++) {
-		tid = Create(NORMAL, &RPSClient_Start);
-		assert(tid == 3 + i + 1, "RPSClient tid not");
+	char receive_buffer[MESSAGE_SIZE];
+	char reply_buffer[MESSAGE_SIZE];
+	TestMessage * receive_message = (TestMessage *) receive_buffer;
+	TestMessage * reply_message = (TestMessage *) reply_buffer;
+	int source_tid;
+
+	while (1) {
+		Receive(&source_tid, receive_buffer,MESSAGE_SIZE);
+		assert(source_tid == 2, "Source tid wasnt 2");
+
+		if (receive_message->message_type == MESSAGE_TYPE_SHUTDOWN) {
+			break;
+		}
+
+		assert(receive_message->message_type == MESSAGE_TYPE_PLAY, "1st task didn't get PLAY message");
+		robprintfbusy((const unsigned char *)"First: received\n");
+		reply_message->message_type = MESSAGE_TYPE_ACK;
+		Reply(2, reply_buffer, MESSAGE_SIZE);
+		robprintfbusy((const unsigned char *)"First: replied\n");
+		Pass();
 	}
+	
+	robprintfbusy((const unsigned char *)"First: received shutdown\n");
+	reply_message->message_type = MESSAGE_TYPE_ACK;
+	Reply(2, reply_buffer, MESSAGE_SIZE);
+	robprintfbusy((const unsigned char *)"First: replied\n");
+	robprintfbusy((const unsigned char *)"First: shutdown\n");
 	
 	Exit();
 	
 	assert(0, "Shouldn't see me\n");
 }
+
+void SecondTask_Start() {
+	char send_buffer[MESSAGE_SIZE];
+	char reply_buffer[MESSAGE_SIZE];
+	TestMessage * send_message = (TestMessage *) send_buffer;
+	TestMessage * reply_message = (TestMessage *) reply_buffer;
+	unsigned int count = 0;
+	while (count < 100) {
+		send_message->message_type = MESSAGE_TYPE_PLAY;
+		Send(1, send_buffer, MESSAGE_SIZE, reply_buffer, MESSAGE_SIZE);
+		assert(reply_message->message_type == MESSAGE_TYPE_ACK, "second task didn't get an ACK message");
+		robprintfbusy((const unsigned char *)"Second: sent\n");
+		Pass();
+		count ++;
+	}
+
+	send_message->message_type = MESSAGE_TYPE_SHUTDOWN;
+	Send(1, send_buffer, MESSAGE_SIZE, reply_buffer, MESSAGE_SIZE);
+	assert(reply_message->message_type == MESSAGE_TYPE_ACK, "second task didn't get an ACK message");
+	robprintfbusy((const unsigned char *)"Second: shutdown\n");
+
+	Exit();
+
+}
+
 
 int overflow(int times){
 	robprintfbusy((const unsigned char *)"Doing overflow in task %d with time as %d\n",times, MyTid());
--- io/kernel2/tasks.h
+++ io/kernel2/tasks.h
@@ -1,9 +1,16 @@
+#include "message.h"
 #ifndef TASK_H_
 #define TASK_H_
+
+typedef struct TestMessage {
+	MessageType message_type;
+} TestMessage;
 
 void KernelTask_Start();
 
 void FirstTask_Start();
+
+void SecondTask_Start();
 
 
 #endif
