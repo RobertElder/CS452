ffbb2dffa1846a877ccdf8a7c83d618bebd1e6ba
Robert Elder <robert@robertelder.ca>
2013-06-18 18:30:52 -0400

    Things are looking good for preemptive context switching.

--- io/kernel4/kernel_irq.h
+++ io/kernel4/kernel_irq.h
@@ -4,29 +4,29 @@
 #ifndef KERNEL_IRQ_H_
 #define KERNEL_IRQ_H_
 
-static int * const timer_ldr = (int*)(TIMER3_BASE + LDR_OFFSET);
-static int * const timer_val = (int*)(TIMER3_BASE + VAL_OFFSET);
-static int * const timer_ctrl = (int*)(TIMER3_BASE + CRTL_OFFSET);
-static int * const timer_clear = (int*)(TIMER3_BASE + CLR_OFFSET);
-static const int TC3OI = 51;
-static const int UART1RXINTR1 = 23;
-static const int UART1TXINTR1 = 24;
-static const int UART2RXINTR2 = 25;
-static const int UART2TXINTR2 = 26;
-static int * const VIC1BaseAddress = (int *) 0x800B0000;
-static int * const VIC1IntSelect = (int *)0x800B000C;
-static int * const VIC1IntEnable = (int *)0x800B0010;
-static int * const VIC1VectAddr = (int *)0x800B0030;
-static int * const VIC1IntEnClear = (int *)0x800B0014;
-static int * const VIC1SoftInt = (int*) 0x800B0018;
-static int * const VIC1SoftIntClear = (int*) 0x800B001C;
-static int * const VIC2BaseAddress = (int *) 0x800C0000;
-static int * const VIC2IntSelect = (int *)0x800C000C;
-static int * const VIC2IntEnable = (int *)0x800C0010;
-static int * const VIC2VectAddr = (int *)0x800C0030;
-static int * const VIC2IntEnClear = (int *)0x800C0014;
-static int * const VIC2SoftInt = (int*) 0x800C0018;
-static int * const VIC2SoftIntClear = (int*) 0x800C001C;
+volatile static int * const timer_ldr = (int*)(TIMER3_BASE + LDR_OFFSET);
+volatile static int * const timer_val = (int*)(TIMER3_BASE + VAL_OFFSET);
+volatile static int * const timer_ctrl = (int*)(TIMER3_BASE + CRTL_OFFSET);
+volatile static int * const timer_clear = (int*)(TIMER3_BASE + CLR_OFFSET);
+volatile static const int TC3OI = 51;
+volatile static const int UART1RXINTR1 = 23;
+volatile static const int UART1TXINTR1 = 24;
+volatile static const int UART2RXINTR2 = 25;
+volatile static const int UART2TXINTR2 = 26;
+volatile static int * const VIC1BaseAddress = (int *) 0x800B0000;
+volatile static int * const VIC1IntSelect = (int *)0x800B000C;
+volatile static int * const VIC1IntEnable = (int *)0x800B0010;
+volatile static int * const VIC1VectAddr = (int *)0x800B0030;
+volatile static int * const VIC1IntEnClear = (int *)0x800B0014;
+volatile static int * const VIC1SoftInt = (int*) 0x800B0018;
+volatile static int * const VIC1SoftIntClear = (int*) 0x800B001C;
+volatile static int * const VIC2BaseAddress = (int *) 0x800C0000;
+volatile static int * const VIC2IntSelect = (int *)0x800C000C;
+volatile static int * const VIC2IntEnable = (int *)0x800C0010;
+volatile static int * const VIC2VectAddr = (int *)0x800C0030;
+volatile static int * const VIC2IntEnClear = (int *)0x800C0014;
+volatile static int * const VIC2SoftInt = (int*) 0x800C0018;
+volatile static int * const VIC2SoftIntClear = (int*) 0x800C001C;
 
 void irq_handler();
 
--- io/kernel4/public_kernel_interface.c
+++ io/kernel4/public_kernel_interface.c
@@ -219,7 +219,8 @@ void PutWord(int channel, int n, char fc, char *bf ) {
 
 int PutString( int channel, const char * fmt, ...) {
 	// TODO: send a message to the whatever server
-	
+	/*
+	 * TODO:  This does not compile on O0
 	char bf[12];
 	char ch, lz;
 	int w;
@@ -277,7 +278,6 @@ int PutString( int channel, const char * fmt, ...) {
 	}
 	va_end(va);
 		
-	/*
 	va_list va;
 	va_start(va,message);
 	bwformatbusy( (const unsigned char *) message, va );
