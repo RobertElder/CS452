RobertElder/CS452

commit dd2853f8292acb88f24e28ea3d87aba80952c9c5

Add in basic sensor attribution

io/project2/train_logic.c
@@ -55,7 +55,7 @@ void TrainServer_ProcessEngineIdle(TrainServer * server, TrainEngine * engine) {
 }
 
 void TrainServer_ProcessEngineFindingPosition(TrainServer * server, TrainEngine * engine) {
-	track_node * node = TrainServer_GetEnginePosition(server);
+	track_node * node = TrainServer_GetEnginePosition(server, engine);
 	
 	if (node && engine->current_node != node) {
 		engine->state = TRAIN_ENGINE_FOUND_STARTING_POSITION;
@@ -153,7 +153,7 @@ int DistanceToDestination(TrainEngine * engine) {
 }
 
 void TrainServer_ProcessEngineRunning(TrainServer * server, TrainEngine * engine) {
-	track_node * node = TrainServer_GetEnginePosition(server);
+	track_node * node = TrainServer_GetEnginePosition(server, engine);
 	
 	if (node && node != engine->current_node) {
 		engine->current_node = node;
@@ -259,15 +259,25 @@ void TrainServer_ProcessEngineReverseAndTryAgain(TrainServer * server, TrainEngi
 	TrainServer_ProcessEngineFindingPosition(server, engine);
 }
 
-track_node * TrainServer_GetEnginePosition(TrainServer * server) {
-	// TODO, need to sensor attribution better
+track_node * TrainServer_GetEnginePosition(TrainServer * server, TrainEngine * engine) {
 	int sensor_module;
 	int sensor_num;
 	
 	for (sensor_module = 0; sensor_module < NUM_SENSOR_MODULES; sensor_module++) {
 		for (sensor_num = 0; sensor_num < SENSORS_PER_MODULE; sensor_num++) {
 			if ((server->sensor_bit_flags[sensor_module] >> sensor_num) & 0x01) {
-				return SensorToTrackNode(server->current_track_nodes, sensor_module, sensor_num);
+				track_node * candidate_node = SensorToTrackNode(server->current_track_nodes, sensor_module, sensor_num);
+				
+				if (!engine->route_nodes_length) {
+					return candidate_node;
+				} 
+				// TODO: need to sensor attribution better
+				// this only checks if the node is on the path
+				int i;
+				for (i = 0; i < engine->route_nodes_length; i++) {
+					engine->route_node_info[i].node = candidate_node;
+					return candidate_node;
+				}
 			}
 		}
 	}

io/project2/train_logic.h
@@ -28,7 +28,7 @@ void TrainServer_ProcessEngineWaitAndGoForever(TrainServer * server, TrainEngine
 
 void TrainServer_ProcessEngineReverseAndTryAgain(TrainServer * server, TrainEngine * engine);
 
-track_node * TrainServer_GetEnginePosition(TrainServer * server);
+track_node * TrainServer_GetEnginePosition(TrainServer * server, TrainEngine * engine);
 
 void TrainServer_SetTrainSpeed(TrainServer * server, int speed, int train_num);
 

commit c8bcc3ae96ec19e877b69de971d40d52160c1be1

Fix bug where route_node_info was overwritten introduced in commit dd2853f8

io/project2/route.c
@@ -36,6 +36,7 @@ void ReserveTrackNode(track_node * node, int train_num){
 	assertf((node->reverse->reserved == 0 || node->reverse->reserved == train_num),"Attempting to reserve node %s that is reserved to %d.", node->reverse->name, train_num);
 	node->reserved = train_num;
 	node->reverse->reserved = train_num;
+	//PrintMessage("Reserve %s %s by %d", node->name, node->reverse->name, train_num);
 }
 
 void ReleaseTrackNode(track_node * node, int train_num){
@@ -43,6 +44,7 @@ void ReleaseTrackNode(track_node * node, int train_num){
 	assertf((node->reverse->reserved == train_num || node->reverse->reserved == 0),"Attempting to release node %s that is reserved to %d instead of %d.", node->reverse->name,node->reverse->reserved, train_num);
 	node->reserved = 0;
 	node->reverse->reserved = 0;
+	//PrintMessage("Release %s %s by %d", node->name, node->reverse->name, train_num);
 }
 
 void ReleaseTrackNodes(TrainEngine * engine) {

io/project2/train_logic.c
@@ -293,8 +293,9 @@ track_node * TrainServer_GetEnginePosition(TrainServer * server, TrainEngine * e
 				// this only checks if the node is on the path
 				int i;
 				for (i = 0; i < engine->route_nodes_length; i++) {
-					engine->route_node_info[i].node = candidate_node;
-					return candidate_node;
+					if (engine->route_node_info[i].node == candidate_node) {
+						return candidate_node;
+					}
 				}
 			}
 		}