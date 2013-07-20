a8abfe9963c1a739b7887a89a4c4d0a701ea562e
Robert Elder <robert@robertelder.ca>
2013-07-20 11:26:12 -0400

    Stuff for test cases.

--- io/project2/tracks/tests/tests.c
+++ io/project2/tracks/tests/tests.c
@@ -2,5 +2,8 @@
 #include "../track_data.h"
 
 int main() {
-
+	track_node track_a_nodes[TRACK_MAX];
+	track_node track_b_nodes[TRACK_MAX];
+	init_tracka(track_a_nodes);
+	init_trackb(track_b_nodes);
 }
--- io/project2/tracks/track_data.c
+++ io/project2/tracks/track_data.c
@@ -1,12 +1,15 @@
 /* THIS FILE IS GENERATED CODE -- DO NOT EDIT */
 
 #include "track_data.h"
-
+#ifdef TESTS
+#include <string.h>
+#else
 static void *memset(void *s, int c, unsigned int n) {
   unsigned char *p = s;
   while(n --> 0) { *p++ = (unsigned char)c; }
   return s;
 }
+#endif
 
 void init_tracka(track_node *track) {
   memset(track, 0, TRACK_MAX*sizeof(track_node));
